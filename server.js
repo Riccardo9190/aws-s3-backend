27def3a2a16efc8f9b38506bdfa4c393392965b1
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2023-07-28 17:02:23 -0300

    Added uploadFile config

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const router = require("./routes");
 
 const app = express();
 
 app.use(cors());
+app.use(router);
 
 app.listen(8080, () => {
   console.log("Listen port 8080");
